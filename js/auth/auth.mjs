d88795bd6a049cffca1c98ed96c5bfcfba6513a3
StineNygren <107799331+StineNygren@users.noreply.github.com>
2023-10-04 14:43:19 +0200

    fetch for login and signup

--- js/auth/auth.mjs
+++ js/auth/auth.mjs
@@ -1,1 +1,104 @@
+const signUpBtn = document.querySelector(".sign-up-btn");
+const logInBtn = document.querySelector(".log-in-btn");
+const SignUpBox = document.querySelector(".sign-up-box");
+const LoggInBox = document.querySelector(".logg-in-box");
+const SignInBtn2 = document.querySelector(".sign-in-btn-2");
+const SignInBtn1 = document.querySelector(".sign-in-btn-1");
+
+signUpBtn.addEventListener("click", () => {
+  SignUpBox.classList.toggle("d-none");
+  LoggInBox.classList.toggle("d-none");
+});
+
+logInBtn.addEventListener("click", () => {
+  SignUpBox.classList.toggle("d-none");
+  LoggInBox.classList.toggle("d-none");
+});
+
+// login
+
+import { baseURL } from "../env/env.mjs";
+
+const usernameInput = document.querySelector("#signup-username");
+const emailInput = document.querySelector("#signup-email");
+const confirmPasswordInput = document.querySelector("#signup-confirm-Password");
+
+console.log(baseURL);
+
+async function logIn() {
+  try {
+    const response = await fetch(`${baseURL}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: emailInput.value,
+        password: confirmPasswordInput.value,
+      }),
+    });
+    console.log(emailInput.value);
+    console.log(confirmPasswordInput.value);
+    if (!response.ok) {
+      throw new Error("Failed to fetch api. Status: " + response.status);
+    }
+    const result = await response.json();
+    saveData(result);
+
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function saveData(result) {
+  let token = result.accessToken;
+  localStorage.setItem("jwt", `Bearer ` + token);
+  console.log(result.accessToken);
+}
+
+//signup
+
+async function signUp() {
+  try {
+    const response = await fetch(`${baseURL}/auth/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: usernameInput.value,
+        email: emailInput.value,
+        password: confirmPasswordInput.value,
+      }),
+    });
+    if (!response.ok) {
+      throw new Error("Failed to fetch api. Status: " + response.status);
+    }
+
+    const result = await response.json();
+
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function signIn() {
+  await signUp();
+  await logIn();
+}
+
+SignInBtn1.addEventListener("click", (e) => {
+  e.preventDefault();
+  signIn();
+});
+SignInBtn2.addEventListener("click", (e) => {
+  e.preventDefault();
+  logIn();
+});
+
 export { signUpBtn, logInBtn };
+
+//localStorage.clear()
