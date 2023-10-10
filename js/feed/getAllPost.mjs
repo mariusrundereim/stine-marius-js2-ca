756dbd54fb5906d72b2812289279fcdc215a78cb
StineNygren <107799331+StineNygren@users.noreply.github.com>
2023-10-10 12:21:06 +0200

    show follower and following

--- js/feed/getAllPost.mjs
+++ js/feed/getAllPost.mjs
@@ -25,7 +25,7 @@ async function getAllPosts() {
 
       feedAllPosts.innerHTML += `
       
-      <div class="border border-dark border-opacity-25 rounded mb-4">
+      <div class="border border-dark border-opacity-25 rounded mb-4 tirsdag-test">
       <div class="col ratio ratio-1x1 bg-dark rounded-top">
       <img src="${post.media}" alt=""></div>
       <div class="col bg-white p-2">
--- js/pages/feedPage/feed.mjs
+++ js/pages/feedPage/feed.mjs
@@ -1,6 +1,9 @@
 import { sendNewPost } from "../../src/utils/domElements.mjs";
 import { newPost } from "../../feed/newPost.mjs";
 import { getAllPosts } from "../../feed/getAllPost.mjs";
+import { getFollowing } from "../../feed/following.mjs";
+
+getFollowing();
 
 sendNewPost.addEventListener("click", () => {
   newPost();
