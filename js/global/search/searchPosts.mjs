98de034fe1d9e01e716a913e409bb5be3c547a37
mariusrundereim <marius.rundereim@gmail.com>
2023-10-10 10:45:32 +0200

    dealing with null

--- js/global/search/searchPosts.mjs
+++ js/global/search/searchPosts.mjs
@@ -1,18 +1,11 @@
 import { baseURL } from "../../env/env.mjs";
-import {
-  jwt,
-  feedAllPosts,
-  inputSearchTitle,
-} from "../../src/utils/domElements.mjs";
+import { jwt } from "../../src/utils/domElements.mjs";
 
-// const inputSearchTitle = document.querySelector("#search-input-modal");
-// const inputSelectionUsername = document.querySelector("#usernameRadio");
-// const inputSelectionHashtag = document.querySelector("#hashtagRadio");
+const inputSearchTitle = document.querySelector("#search-input-modal");
+const inputSelectionUsername = document.querySelector("#usernameRadio");
+const inputSelectionHashtag = document.querySelector("#hashtagRadio");
 
 export function searchTest() {
   console.log(inputSearchTitle);
-  inputSearchTitle.addEventListener("keyup", () => {
-    console.log("clicked");
-  });
+  console.log("Hei");
 }
-searchTest();
