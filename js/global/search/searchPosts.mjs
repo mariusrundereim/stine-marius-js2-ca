731f70b89557c549c07a338bd129872ecd8242cc
mariusrundereim <marius.rundereim@gmail.com>
2023-10-11 16:24:00 +0200

    error search

--- js/global/search/searchPosts.mjs
+++ js/global/search/searchPosts.mjs
@@ -1,33 +1,38 @@
 import { baseURL } from "../../env/env.mjs";
 import { jwt } from "../../src/utils/domElements.mjs";
 
+export function displaySelection(results) {
+  console.log("DOM", results);
+}
+
+export async function searchPostsByTag(tag) {
+  try {
+    const response = await fetch(`${baseURL}/posts?_tag=${tag}`);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error", error);
+    return [];
+  }
+}
+
 export function searchTest() {
   const inputSearchTitle = document.querySelector("#search-input-modal");
   const inputSelectionUsername = document.querySelector("#usernameRadio");
   const inputSelectionHashtag = document.querySelector("#hashtagRadio");
 
-  inputSearchTitle.addEventListener("keyup", (e) => {
+  inputSearchTitle.addEventListener("keyup", async (e) => {
     const searchValue = inputSearchTitle.value;
     const selectedOption = inputSelectionUsername.checked
       ? "Username"
       : "Hashtag";
-    console.log(`Search ${searchValue} with option ${selectOption}`);
+    if (selectedOption === "Hashtag" && searchValue) {
+      const results = await searchPostsByTag(searchValue);
+      console.log(results);
+      displaySelection(results);
+    }
   });
-
-  inputSelectionUsername.addEventListener("change", searchOptionTest);
-  inputSelectionHashtag.addEventListener("change", searchOptionTest);
-}
-
-export function searchOptionTest(e) {
-  const selectOption = e.target.value;
-
-  if (selectOption === "option1") {
-    console.log("option1 selected");
-  } else if (selectOption === "option2") {
-    console.log("opt2 selected");
-  }
-}
-
-export function displaySelection() {
-  const displayElements = document.querySelector(".elements-container");
 }
--- js/script.mjs
+++ js/script.mjs
@@ -1,6 +1,10 @@
 import { Header } from "./layout/Header.mjs";
 import { searchModal } from "./global/search/searchfile.mjs";
-import { searchTest } from "./global/search/searchPosts.mjs";
+import {
+  searchTest,
+  searchPostsByTag,
+  displaySelection,
+} from "./global/search/searchPosts.mjs";
 
 const headerElement = Header();
 const searchModalEle = searchModal();
@@ -8,6 +12,9 @@ const searchModalEle = searchModal();
 document.body.prepend(headerElement);
 document.body.prepend(searchModalEle);
 searchTest();
+//searchOptionTest();
+searchPostsByTag();
+displaySelection();
 
 const signOut = document.querySelector("#signOut");
 
