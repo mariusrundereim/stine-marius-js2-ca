bed01b6516bbbac9712c3b641c2e8c6b73f91f8d
mariusrundereim <marius.rundereim@gmail.com>
2023-10-11 10:24:13 +0200

    search testing working fine

--- js/global/search/searchPosts.mjs
+++ js/global/search/searchPosts.mjs
@@ -1,11 +1,15 @@
 import { baseURL } from "../../env/env.mjs";
 import { jwt } from "../../src/utils/domElements.mjs";
 
-const inputSearchTitle = document.querySelector("#search-input-modal");
-const inputSelectionUsername = document.querySelector("#usernameRadio");
-const inputSelectionHashtag = document.querySelector("#hashtagRadio");
-
 export function searchTest() {
+  const inputSearchTitle = document.querySelector("#search-input-modal");
+  const inputSelectionUsername = document.querySelector("#usernameRadio");
+  const inputSelectionHashtag = document.querySelector("#hashtagRadio");
+
+  inputSearchTitle.addEventListener("keyup", (e) => {
+    console.log(inputSearchTitle.value);
+  });
+
   console.log(inputSearchTitle);
   console.log("Hei");
 }
--- js/script.mjs
+++ js/script.mjs
@@ -1,8 +1,11 @@
 import { Header } from "./layout/Header.mjs";
 import { searchModal } from "./global/search/searchfile.mjs";
+import { searchTest } from "./global/search/searchPosts.mjs";
 
 const headerElement = Header();
 const searchModalEle = searchModal();
 
 document.body.prepend(headerElement);
 document.body.prepend(searchModalEle);
+
+searchTest();
