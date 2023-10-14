1546870050d3fb638174610e77f70dc7c58203fb
mariusrundereim <marius.rundereim@gmail.com>
2023-10-14 14:45:27 +0200

    dealing with error

--- js/feed/searchPosts.mjs
+++ js/feed/searchPosts.mjs
@@ -2,7 +2,8 @@ import { fetchAllPosts } from "../components/fetchApiPosts.mjs";
 
 export function searchPosts(posts, query) {
   //const filteredPosts = posts.filter((post) => post.title.includes(query));
-  const filteredTitles = posts.filter((title) => title.includes(query));
-  console.log("Filtered titles:", filteredTitles);
-  return filteredTitles;
+  //const filteredTitles = posts.filter((title) => title.includes(query));
+  const filteredPosts = posts.filter((post) => post.title.includes(query));
+  console.log("Filtered titles:", filteredPosts);
+  return filteredPosts;
 }
